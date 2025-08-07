5d0afc760812c6a8b161f9bb4a2dc662b16b84ed
lanjelot <lanjelot@gmail.com>
2025-08-07 12:43:42 +1000

    new method

--- sqli/dao/student.py
+++ sqli/dao/student.py
@@ -38,10 +38,16 @@ class Student(NamedTuple):
             return [Student.from_raw(r) for r in results]
 
     @staticmethod
-    async def create(conn: Connection, name: str):
+    async def create_old(conn: Connection, name: str):
         q = ("INSERT INTO students (name) "
              "VALUES ('%(name)s')" % {'name': name})
         async with conn.cursor() as cur:
             await cur.execute(q)
 
+    @staticmethod
+    async def create(conn: Connection, name: str):
+        q = ("INSERT INTO students (name) "
+             "VALUES ('%(name)s')" % {'name': name})
+        async with conn.cursor() as cur:
+            await cur.execute(q)
 
